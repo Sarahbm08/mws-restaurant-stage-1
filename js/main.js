04d613a0de6e19eb7436a283773402ee055c6b7c
Sarah Morley <the.jephthah@gmail.com>
2018-09-03 20:42:03 -0600

    Restaurant page style clean-up

--- js/main.js
+++ js/main.js
@@ -166,25 +166,31 @@ createRestaurantHTML = (restaurant) => {
   const imgUrl2x = imgArray[0] + '_2x.' + imgArray[1]; //insert '_2x' on image name
   image.src = imgUrl1x; //default to smaller image
   image.srcset = imgUrl1x + ' 300w, ' + imgUrl2x + ' 600w';
-  image.alt = restaurant.name + ' image';
+  image.alt = restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const div = document.createElement('div');
+  div.className = 'restaurants-text-area';
+  li.append(div);
+  
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  div.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  div.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  div.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more);
+  more.onclick = function() {
+	  window.location = DBHelper.urlForRestaurant(restaurant);
+  }
+  div.append(more);
 
   return li;
 }
